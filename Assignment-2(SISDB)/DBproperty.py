8bb0670a288ffe88bb7dd55389f6e089df62bbb1
Utkarsh Mandilwar <75298162+utmandilwar@users.noreply.github.com>
2023-12-26 23:36:08 +0530

    Add files via upload

---
+++ Assignment-2(SISDB)/DBproperty.py
@@ -0,0 +1,7 @@
+class PropertyUtil:
+    def getPropertyString():
+        host = 'localhost'
+        username = 'root'
+        password = 'root'
+        database = 'SISDB'
+        return host, username, password, database
