72671fb8f9fd506e729080e51c538c70059c2b6a
Utkarsh Mandilwar <75298162+utmandilwar@users.noreply.github.com>
2023-12-28 17:36:03 +0530

    Add files via upload

---
+++ Assignment-5/util/DBPropertyUtil.py
@@ -0,0 +1,9 @@
+class PropUtil:
+    def getPropertyString():
+        host = 'localhost'
+        username = 'root'
+        password = 'root'
+        database = 'ticketbookingsystem'
+        port = '3306'
+        auth_plugin='mysql_native_password'
+        return host, username, password, database, port, auth_plugin
